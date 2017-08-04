f698137fafad5d398f14f5979927ef91e78e54d1
RowlandZero <steven.rowland@wework.com>
2017-08-04 15:48:18 -0400

    login/logout added to flow

--- forms.py
+++ forms.py
@@ -1,10 +1,15 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+class SignupForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter a valid email address."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password of at least 6 characters."), Length(min=6, message="Please enter at least 6 characters.")])
     submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired("Please enter your login email address."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password")])
+    submit = SubmitField('Login')
